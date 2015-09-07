e18b79866f273d3b422b19261d8fcdacea58f320
cd10012 <ced361@nyu.edu>
2015-09-07 17:39:21 -0400

    added branch

--- lotus/lotus/ViewController_Profile.swift
+++ lotus/lotus/ViewController_Profile.swift
@@ -8,32 +8,177 @@
 
 import UIKit
 
-class ViewController_Profile: UIViewController {
+let FIELD_FN_TAG = 1
+let FIELD_LN_TAG = 2
+let FIELD_ZIP_TAG = 3
+let FIELD_DAY_TAG = 4
+let FIELD_MON_TAG = 5
+let FIELD_YEAR_TAG = 6
+let FIELD_EMAIL_TAG = 7
 
+class ViewController_Profile: UIViewController, UITextFieldDelegate {
+    
+    //Outlet References
+    
     @IBOutlet weak var buttonNext: UIButton!
+    
     @IBOutlet weak var fnTextField: UITextField!
+    @IBOutlet weak var lnTextField: UITextField!
+    @IBOutlet weak var zipTextField: UITextField!
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        styleTextField(fnTextField, 3.0, UIColor.blackColor().CGColor)
         
-        // Do any additional setup after loading the view.
+        //set tags for TextViewDelegate
+        fnTextField.tag = FIELD_FN_TAG
+        lnTextField.tag = FIELD_LN_TAG
+        zipTextField.tag = FIELD_ZIP_TAG
+        dayTextField.tag = FIELD_DAY_TAG
+        monthTextField.tag = FIELD_MON_TAG
+        yearTextField.tag = FIELD_YEAR_TAG
+        emailTextField.tag = FIELD_EMAIL_TAG
+        
+        
+        //style text fields
+        styleTextField(fnTextField, 1.5, UIColor.whiteColor().CGColor, "First Name")
+        styleTextField(lnTextField, 1.5, UIColor.whiteColor().CGColor, "Last Name")
+        styleTextField(zipTextField, 1.5, UIColor.whiteColor().CGColor, "Zip")
+        styleTextField(dayTextField, 1.5, UIColor.whiteColor().CGColor, "Day")
+        styleTextField(monthTextField, 1.5, UIColor.whiteColor().CGColor, "Month")
+        styleTextField(yearTextField, 1.5, UIColor.whiteColor().CGColor, "Year")
+        styleTextField(emailTextField, 1.5, UIColor.whiteColor().CGColor, "Email")
+        
+        
+        //set delagates to the view controller
+        fnTextField.delegate = self
+        lnTextField.delegate = self
+        zipTextField.delegate = self
+        dayTextField.delegate = self
+        monthTextField.delegate = self
+        yearTextField.delegate = self
+        emailTextField.delegate = self
+        
+        //setting the next field property
+        self.fnTextField.nextField = self.lnTextField
+        self.lnTextField.nextField = self.zipTextField
+        self.zipTextField.nextField = self.dayTextField
+        self.dayTextField.nextField = self.monthTextField
+        self.monthTextField.nextField = self.yearTextField
+        self.yearTextField.nextField = self.emailTextField
+        
+        
+        dayTextField.inputView = UIView(frame: CGRectZero)
+        monthTextField.inputView = UIView(frame: CGRectZero)
+        yearTextField.inputView = UIView(frame: CGRectZero)
+        
+        
+        //set datepicker attributes
+        datePicker.backgroundColor = UIColor.whiteColor()
+        datePicker.maximumDate = NSDate()
+        datePicker.hidden = true
+        
+        //defaulted to false, is set to true after text fields are validated
+        buttonNext.enabled = false
+        
+        
+        var tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func dayFieldShouldBegin(sender: UITextField) {
+        datePicker.hidden = false
+        datePicker.datePickerMode = UIDatePickerMode.Date
+        
+        datePicker.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    @IBAction func yearFieldShouldBegin(sender: UITextField) {
+        datePicker.hidden = false
+        datePicker.datePickerMode = UIDatePickerMode.Date
+        
+        datePicker.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    @IBAction func monthFieldShouldBegin(sender: UITextField) {
+        datePicker.hidden = false
+        datePicker.datePickerMode = UIDatePickerMode.Date
+        
+        datePicker.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        dayTextField.text = getDayFromDate(sender.date)
+        yearTextField.text = getYearFromDate(sender.date)
+        monthTextField.text = getMonthFromDate(sender.date)
+    }
+    
+    @IBAction func dayEndEditing(sender: UITextField) {
+        datePicker.hidden = true
+    }
+    
+    @IBAction func monthEndEditing(sender: UITextField) {
+        datePicker.hidden = true
+    }
+    
+    @IBAction func yearEndEditing(sender: UITextField) {
+        datePicker.hidden = true
+    }
+    
+    //UI Flow methods
+    
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        //next field method
+        if (textField == emailTextField) {
+            textField.resignFirstResponder()
+        }
+        else {
+            textField.nextField?.becomeFirstResponder()
+        }
+        
+        return true
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let text = textField.text ?? ""
+        let decimalSeparator = NSLocale.currentLocale().objectForKey(NSLocaleDecimalSeparator) as! String
+        let testText = (text as NSString).stringByReplacingCharactersInRange(range, withString: string)
+        
+        
+        if(textField.tag == FIELD_ZIP_TAG) {
+            let newLength = count(text.utf16) + count(string.utf16) - range.length
+            return isNumeric(testText)
+        }
+        
+        return true
+    }
+    
+    
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- lotus/lotus/global.swift
+++ lotus/lotus/global.swift
@@ -9,8 +9,20 @@
 import Foundation
 import UIKit
 
+//
+//  global.swift
+//  lotus
+//
+//  Created by Cory Dickson on 8/9/15.
+//
+//
 
-func styleTextField(textField: UITextField, borderWidth: CGFloat, borderColor: CGColor) {
+//
+
+
+func styleTextField(textField: UITextField, borderWidth: CGFloat, borderColor: CGColor, placeHolderText: String) {
+    ///Styles text field with an underline given a UITextField, CGFloat width, and a CGColor for the border color and placeholer text
+    
     let border = CALayer()
     let width = CGFloat(borderWidth)
     border.borderColor = borderColor
@@ -19,6 +31,103 @@ func styleTextField(textField: UITextField, borderWidth: CGFloat, borderColor: C
     border.borderWidth = width
     textField.layer.addSublayer(border)
     textField.layer.masksToBounds = true
+    textField.attributedPlaceholder = NSAttributedString(string: placeHolderText,
+        attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
     
 }
+
+
+//can return date data in dd mmm yyyy formating
+func getDayFromDate(date: NSDate) -> String {
+    //returns day from dd mmm yyyy NSDate formatting
+    var dateFormatter = NSDateFormatter()
+    
+    dateFormatter.dateFormat = "dd MMM yyyy"
+    
+    var dateStr = dateFormatter.stringFromDate(date)
+    let idx = advance(dateStr.startIndex, 2)
+    
+    return dateStr.substringToIndex(idx)
+}
+
+func getMonthFromDate(date: NSDate) -> String {
+    //returns month from dd mmm yyyy NSDate formatting
+    var dateFormatter = NSDateFormatter()
+    
+    dateFormatter.dateFormat = "dd MMM yyyy"
+    
+    var dateStr = dateFormatter.stringFromDate(date)
+    
+    let idx = advance(dateStr.startIndex, 2)
+    
+    let idxM = advance(dateStr.endIndex, -4)
+    
+    var day = dateStr.substringToIndex(idx)
+    var year = dateStr.substringFromIndex(idxM)
+    var month = dateStr.stringByReplacingOccurrencesOfString(day, withString: "")
+    month = month.stringByReplacingOccurrencesOfString(year, withString: "")
+    
+    month = month.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    
+    return month
+    
+}
+
+func getYearFromDate(date: NSDate) -> String {
+    //returns year from dd mmm yyyy NSDate formatting
+    var dateFormatter = NSDateFormatter()
+    
+    dateFormatter.dateFormat = "dd MMM yyyy"
+    
+    var dateStr = dateFormatter.stringFromDate(date)
+    let idx = advance(dateStr.endIndex, -4)
+    
+    return dateStr.substringFromIndex(idx)
+    
+}
+
+func isNumeric(a: String) -> Bool {
+    return a.toInt() != nil
+}
+
+
+extension UIColor {
+    
+    //usage:
+    //var color = UIColor(red: 0xFF, green: 0xFF, blue: 0xFF
+    //var color2= UIColor(netHex: 0xFFFFFF)
+    
+    
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        ///Using int values ranging from [0,255] set UIColor
+        ///red: Int, green: Int, blue: Int
+        
+        assert(red >= 0 && red <= 255, "Invalid red componet")
+        assert(green >= 0 && red <= 255, "Invalid green componet")
+        assert(blue >= 0 && red <= 255, "Invalid blue componet")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex: Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue: netHex & 0xff )
+    }
+}
+
+//expanding UITextField to know what is the "next" text field in the flow
+
+private var kAssociationKeyNextField: UInt8 = 0
+
+extension UITextField {
+    var nextField: UITextField? {
+        get {
+            return objc_getAssociatedObject(self, &kAssociationKeyNextField) as? UITextField
+        }
+        set(newField) {
+            objc_setAssociatedObject(self, &kAssociationKeyNextField, newField, UInt(OBJC_ASSOCIATION_RETAIN))
+        }
+    }
+}
+
 
